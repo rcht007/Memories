11cdfbf37d5308ab25d430d0191afe0582ebafda
rchtnhr007@gmail.com <rchtnhr007@gmail.com>
2021-07-27 11:27:45 +0530

    =added posts

--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import postRouter from './routes/posts.js'
-import {mainPage} from './controlers/Main.js'
+import postRoutes from './routes/posts.js';
 
-const app=express();
+const app = express();
 
-app.use(bodyParser.json({limit:"30mb",extended:true}))
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
-app.use(cors);
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
 
-app.get('/',mainPage)
-app.use('/posts',postRouter)
-
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL=`mongodb+srv://Rachit_3015:Rachit_3015@rachit.olb01.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const PORT=process.env.port||5000;
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-    .then(()=>app.listen(PORT,()=>console.log(`server on port ${PORT}`)))
-    .catch((error)=>console.log(error.message));
-    mongoose.set('useFindAndModify',false);
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
