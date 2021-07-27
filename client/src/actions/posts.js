6a5bbe90fea39db4be0c5371e2b4184ded86347c
rchtnhr007@gmail.com <rchtnhr007@gmail.com>
2021-07-27 12:27:57 +0530

    =updated UI

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,6 +1,6 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
-import * as api from '../api/index.js';
+import * as api from '../API/index.js';
 
 export const getPosts = () => async (dispatch) => {
   try {
--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL=`mongodb+srv://Rachit_3015:Rachit_3015@rachit.olb01.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+const CONNECTION_URL=`mongodb+srv://Rachit_3015:Rachit_3015@rachit.olb01.mongodb.net/Rachit_3015?retryWrites=true&w=majority`;
+// mongodb+srv://Rachit_3015:<password>@rachit.olb01.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
